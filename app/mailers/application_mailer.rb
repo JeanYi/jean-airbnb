259050e3571138b603da90b629f5e62b35f1b383
Mac <mac@Macs-MBP.lan>
2018-06-10 12:42:35 +0800

    installed Clearance gem for user management however unable to install view files

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,3 +2,11 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
 end
+
+
+# 1. Configure the mailer to create full URLs in emails:
+
+#     # config/environments/{development,test}.rb
+#     config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+#     In production it should be your app's domain name.
